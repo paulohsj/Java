a842009d4a48b60c13cc023848f16535090c8857
paulohsj <paulohsj44@gmail.com>
2025-10-02 17:42:51 -0300

    Primeiros passos do desáfio

--- Desafio.java
+++ Desafio.java
@@ -1,3 +1,7 @@
+
+import java.util.Scanner;
+
+
 public class Desafio {
     //Desafio: Criar um App com função de um Banco Digital simples
 
@@ -16,7 +20,40 @@ public class Desafio {
     
     
     public static void main(String[] args) {
-        System.out.println("Desafio concluído com sucesso!");
+        Scanner leitor = new Scanner(System.in);
+
+        //Iniciando dados do cliente
+        System.out.println("**************************");
+        System.out.println("Bem-vindo ao Banco Digital");
+        System.out.println("Nome do Cliente: Fulano de Tal");
+        String tipoDeConta = "Conta Corrente";
+        System.out.println("Tipo de Conta: " + tipoDeConta);
+        double saldoDaConta = 1000.00;
+        System.out.println("Seu Saldo é de: R$ " + String.format("%.2f", saldoDaConta));
+        System.out.println("**************************");
+
+        //Menu de opções
+        System.out.println("Selecione uma das opções abaixo: ");
+        System.out.println("""
+        1 - Ver Saldo
+        2 - Depositar
+        3 - Transferir
+        4 - Sair 
+        """);
+        int opcao = leitor.nextInt();
+
+            switch (opcao) {
+                case 1: 
+                    System.out.println("Seu saldo atual é de: R$ " + String.format("%.2f", saldoDaConta));
+                break;
+                case 2:
+                    System.out.println("Qual valor você deseja depositar? ");
+                    double valorDeposito = leitor.nextDouble();
+                    System.out.println("Você Depositou: R$ " + String.format("%.2f", valorDeposito));
+                    System.out.println("Seu saldo atualizado é de: R$ " + String.format("%.2f", saldoDaConta + valorDeposito));
+                    break;
+                case 3:
+        
+            }
+        }
     }
-    
-}
